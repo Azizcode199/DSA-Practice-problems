ed3179f9d651dca63e27568e8287922ee1f105e9
aziz <azizcode92@gmail.com>
2025-04-06 01:40:15 +0200

    InsertSort

---
+++ JavaScript/InsertSort.js
@@ -0,0 +1,18 @@
+let arr = [25, 17, 31, 13, 2];
+
+function InsertSort() {
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+InsertSort();
