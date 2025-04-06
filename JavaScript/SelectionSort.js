095aa0bc3cefded2497cca0c25728eaf7d425d4b
aziz <azizcode92@gmail.com>
2025-04-07 00:23:20 +0200

    SelectionSort

--- JavaScript/SelectionSort.js
+++ JavaScript/SelectionSort.js
@@ -4,3 +4,30 @@
 // Swap it with the first element.
 
 // Repeat with the rest of the list.
+
+// 💻 Task 1 – Selection Sort:
+// Use Selection Sort to sort this list:
+// [4, 1, 6, 3, 2]
+let arr = [4, 1, 6, 3, 2];
+
+function SelectionSort() {
+  for (let i = 0; i < arr.length-1; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Sort: ${arr[i]}`);
+  }
+}
+
+SelectionSort();
