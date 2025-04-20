6c8f885c432b8fb33c1d51d0b57e51d6ad39f1ab
aziz <azizcode92@gmail.com>
2025-04-20 20:29:00 +0200

    abstrukt + Interface

--- Java/oops/abstract_Interface/Test.java
+++ Java/oops/abstract_Interface/Test.java
@@ -3,3 +3,32 @@ package abstract_Interface;
 interface Elektro {
     void laden(); // keine Logik hier
 }
+abstract class Fahrzeug {
+    String name;
+
+    public Fahrzeug(String name) {
+        this.name = name;
+    }
+
+    abstract void starten(); // abstrakte Methode
+}
+class Auto extends Fahrzeug implements Elektro {
+    public Auto(String name) {
+        super(name);
+    }
+
+    public void starten() {
+        System.out.println(name + " startet mit Schlüssel.");
+    }
+
+    public void laden() {
+        System.out.println(name + " lädt die Batterie.");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Auto tesla = new Auto("Tesla");
+        tesla.starten(); // von abstrakter Klasse
+        tesla.laden();   // von Interface
+    }
+}
