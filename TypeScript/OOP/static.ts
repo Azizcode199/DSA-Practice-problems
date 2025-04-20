c37c73420cfcde921b265b1babc7e2886ef13917
aziz <azizcode92@gmail.com>
2025-04-20 02:20:00 +0200

    static

---
+++ TypeScript/OOP/static.ts
@@ -0,0 +1,24 @@
+// class Auto {
+//     static markt: string = "BMW"
+// }
+
+// console.log(Auto.markt)
+
+// class Auto {
+//     static  zeigeMarke(): void{
+//         console.log("Marke ist BMW")
+//     }
+// }
+
+// Auto.zeigeMarke();
+
+class Auto {
+    static Motor = {
+        info(): void {
+            console.log("Motor läuft");
+        }
+    }
+}
+
+Auto.Motor.info(); // ✅ Ausgabe: Motor läuft
+
