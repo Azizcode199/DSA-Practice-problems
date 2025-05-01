df1a0074ebf65596aa24c2d94640071b774ff866
aziz <azizcode92@gmail.com>
2025-05-01 23:54:37 +0200

    class, Inheritance, Access Modifiers, shorthand properties, seter, geter

--- TypeScript/OOP/class.ts
+++ TypeScript/OOP/class.ts
@@ -1,20 +1,37 @@
-// class, Inheritance, Access Modifiers, shorthand properties
+// class, Inheritance, Access Modifiers, shorthand properties, seter, geter 
 class Persons {
-    private name:string;
-    private age: number;
-    private hobbies: string[];
+  private name: string;
+  private age!: number;
+  private hobbies: string[];
 
-    constructor(name : string, age : number, hobbies:string[]){
-        this.name = name
-        this.age = age
-        this.hobbies = hobbies
+  constructor(name: string, age: number, hobbies: string[]) {
+    this.name = name;
+    this.hobbies = hobbies;
+    this.ageValue = age; // ✅ nutzt den Setter mit Prüfung
+  }
+
+  // Setter für Alter
+  public set ageValue(age: number) {
+    if (age > 125 || age === undefined) {
+      console.log("Das Alter ist ungültig! Bitte gib ein Alter ≤ 125 an.");
+    } else {
+      this.age = age;
     }
+  }
 
-    
-    public vorstellung(): string {
-        return `Hallo, ich bin ${this.name}, bin ${this.age} Jahre alt und liebe ${this.hobbies.join(', ')}.`;
+  // Getter für Alter
+  public get ageValue(): string {
+    if (this.age > 125 || this.age === undefined) {
+      return "Ungültiges Alter! Es darf nicht größer als 125 sein.";
     }
+    return `Das Alter ist: ${this.age}`;
+  }
 
+  public vorstellung(): string {
+    return `Hallo, ich bin ${this.name}, bin ${
+      this.age
+    } Jahre alt und liebe ${this.hobbies.join(", ")}.`;
+  }
 }
 
 class Students extends Persons {
@@ -28,14 +45,16 @@ class Students extends Persons {
     vorstellung(): string {
         return `${super.vorstellung()}Meine Note ist ${this.grade}.`;
     }
-    
-    
+
 }
 
-const person1: Persons = new Persons("Baktash", 35, ["walking", "Fitness"]);
+const person1: Persons = new Persons("Baktash", 25, ["walking", "Fitness"]);
 const person2 = new Persons("Mike", 35, ["Boxing", "Footbal"]);
-const std = new Students("Thoma", 35, ["Coder", "Fitness"] ,2);
+// const std = new Students("Thoma", 35, ["Coder", "Fitness"] ,2);
+console.log(person1.vorstellung());
 
-console.log(person1.vorstellung()); 
-console.log("|----Inheritance----|")
-console.log(std.vorstellung())
+console.log(person1.ageValue)
+
+
+
+
